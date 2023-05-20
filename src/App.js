6d4f661d01f7103e0aa0450ca99d334492feca98
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-05-20 01:53:14 -0400

    implement providers and remove default code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Providers } from './Providers/Providers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Providers>
+
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <button className="actionButton" disabled={true}>Click Me!</button>
+          </div>
+          <div>
+            <button disabled={true}>Click Me!</button>
+          </div>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Providers>
   );
 }
 
