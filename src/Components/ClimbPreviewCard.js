92db468ea7c8f0f57027c26d14cf4661373e0e86
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-05-20 19:29:54 -0400

    improved preview card

--- src/Components/ClimbPreviewCard.js
+++ src/Components/ClimbPreviewCard.js
@@ -1,19 +1,31 @@
 import "../shared-components/styles.css";
 import { createUseStyles } from "react-jss";
 export function ClimbPreviewCard({climbName, climbSetter,climbDesc, climbDiff, publicKey, ...props}){
-
+    const classes = useStyles();
     return (<div className="climb-preview-card">
-        <p className={styles.difficulty}>{climbDiff}</p> 
+        <p className={classes.difficulty} tooltip={'The difficulty of the climb'}>{climbDiff}</p> 
+        <div className={classes["inner-card"]}>
+            <h1>{climbName}</h1>
+            <h2>{climbSetter}</h2>
+            <p>{climbDesc}</p>
+            <button className="action-button widest transition-action">Buy Climb</button>
+        </div>
     </div>)
 }
 
-const styles = createUseStyles({
+const useStyles = createUseStyles({
     "difficulty": {
         fontFamily: 'Franklin Gothic Medium',
         color:'white',
-        fontSize: '40',
-        top: '-6',
-        left: '18',
+        fontSize: '30px',
+        top: '18px',
+        left: '18px',
         position: "absolute",
+    },
+    "inner-card": {
+        padding: '60px 10px',
+        textAlign: 'right',
+        alignItems: 'center',
+        flexDirection: 'column',
     }
 });
