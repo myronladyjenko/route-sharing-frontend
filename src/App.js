35dee3225b5d2f4202224a9e32fd527b66b392d1
TheAngryPopo <joelharder4@gmail.com>
2023-05-20 14:45:02 -0400

    navbar progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import './shared-components/styles.css'
 import { Providers } from './Providers/Providers';
+<<<<<<< Updated upstream
 import { AppRoutes } from './AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,6 +13,41 @@ function App() {
         <AppRoutes/>
       </Providers>
     </BrowserRouter>
+=======
+import { Navbar } from './Navbar/Navbar';
+
+function App() {
+  return (
+
+    <Providers>
+
+      <div className="navbar">
+        <Navbar></Navbar>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <button className="actionButton" disabled={true}>Click Me!</button>
+          </div>
+          <div>
+            <button className="action-button button-transition" disabled={true}>Click Me!</button>
+          </div>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Providers>
+>>>>>>> Stashed changes
   );
 }
 
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,1 +1,25 @@
-import { Router, NavLink, BrowserRouter } from "react-router-dom";
+//import { Router, NavLink, BrowserRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import '../shared-components/styles.css';
+
+
+export function Navbar() {
+    // const [count, setCount] = useState(0);
+
+    // function handleClick() {
+    //   setCount(count + 1);
+    // }
+
+    // <button onClick={handleClick}>
+    //    Clicked {count} times
+    // </button>
+
+    return (
+        <div>
+            <a className='navlink'>Home</a>
+            <a className='navlink'>Browse</a>
+            <a className='navlink'>Create</a>
+            <a className='navlink'>My Collection</a>
+        </div>
+    );
+}
