80a712dd18f783708b396892cb4d33c6ca9b1fe8
TheAngryPopo <joelharder4@gmail.com>
2023-05-21 01:56:25 -0400

    vthgfdhgfxbgfvmhjg

--- src/Screens/Homepage.jsx
+++ src/Screens/Homepage.jsx
@@ -3,13 +3,20 @@ import "../shared-components/styles.css";
 import { createUseStyles } from "react-jss";
 
 import Lol from "../Components/HoldSelectionList";
+const options = [
+  {value: "Peaches"},
+  {value: "Peaches"},
+  {value: "Peaches"},
+  {value: "Peaches"},
+  {value: "Peaches"},
+];
 
 export function Home() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>CLIMBING FOR EVERYONE</h1>
       <button className="action-button">Browse Climbs</button>
-      <Lol placeHolder={"Select Something"}></Lol>
+      <Lol placeHolder={"Select Something"} optionalCallExpression={options}></Lol>
     </div>
   );
 }
