abdd47c40a08ef819c41dc619e83f3f92d495a67
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-05-20 22:59:03 -0400

    prepared editor for getting climbs

--- src/Screens/Editor.jsx
+++ src/Screens/Editor.jsx
@@ -1,26 +1,47 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useServer } from "../Providers/ServerContext";
 import { ClimbPreviewCard } from "../Components/ClimbPreviewCard";
 
 import axios from "axios";
 
-function getClimbs(baseURL) {
+function getClimbs() {
   if (sessionStorage.length === 0) {
     fetchClimb(baseURL);
   }
 }
 
-function fetchClimb(baseURL) {
-  axios
-    .get(`${baseURL}climbConfigs`)
-    .then((response) => {
-      sessionStorage.setItem("holds", JSON.stringify(response.data));
-      console.log(response.data);
-    })
-    .catch((error) => console.log(error));
-}
 export function Editor() {
+  const [holdType, setHoldType] = useState("none");
   const baseURL = useServer();
+  if (holdType === "none") {
+    console.log(JSON.parse(sessionStorage.getItem("holds")));
+    return (
+      <div>
+        <h1>Select a hold type to use</h1>
+        <div>
+          {Object.entries(JSON.parse(sessionStorage.getItem("holds"))).map(
+            ([holdType, arr], index) => {
+              <button key={index} onClick={() => setHoldType(holdType)}>
+                {holdType}
+              </button>;
+            }
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  function fetchClimb() {
+    axios
+      .get(`${baseURL}climbConfigs`)
+      .then((response) => {
+        sessionStorage.setItem("holds", JSON.stringify(response.data));
+        console.log(response.data);
+        console.log(sessionStorage.getItem("holds"));
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div>
       <ClimbPreviewCard
@@ -30,7 +51,7 @@ export function Editor() {
         climbDiff={"V0/4"}
         publicKey={"192ohfkwhdsaif"}
       />
-      <button onClick={() => getClimbs(baseURL)}>Get Climbs</button>
+      <button onClick={fetchClimb}>Get Climbs</button>
     </div>
   );
 }
