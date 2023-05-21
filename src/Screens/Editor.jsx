80edf8496823c2352eed7b7b0546a625abce5ed3
TheAngryPopo <joelharder4@gmail.com>
2023-05-20 22:27:20 -0400

    Update Editor.jsx

--- src/Screens/Editor.jsx
+++ src/Screens/Editor.jsx
@@ -19,18 +19,61 @@ function fetchClimb(baseURL) {
     })
     .catch((error) => console.log(error));
 }
+
+function postClimb() {
+
+  axios.post("/create-climb", {
+    name: "omega epic climb",
+    difficulty: "V4/6B+",
+    author: "Your mother",
+    region: "hell",
+    width: 500,
+    height: 1000,
+    angle: 69,
+    hold_theme: "Mesa Biome",
+    holds: [{id: 1, x: 20, y: 20, rotation: 90}, {id: 2, x: 69, y: 420, rotation: 0}, {id: 3, x: 42, y: 5, rotation: 34}, {id: 7, x: 236, y: 5, rotation: 123}]
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+}
+
 export function Editor() {
   const baseURL = useServer();
   return (
     <div>
-      <ClimbPreviewCard
+      {/* <ClimbPreviewCard
         climbName={"Climb Name"}
         climbDesc={"A lol"}
         climbSetter={"Nathan's balls"}
         climbDiff={"V0/4"}
         publicKey={"192ohfkwhdsaif"}
       />
-      <button onClick={() => getClimbs(baseURL)}>Get Climbs</button>
+      <button onClick={() => getClimbs(baseURL)}>Get Climbs</button> */}
+      <button onClick={() => postClimb()}>Send test climb</button>
     </div>
   );
 }
+
+
+
+{/*
+Name of climb - string
+Difficulty    - string
+Authour       - string
+Region        - string
+board width    - int
+board height   - int
+angle          - int
+hold theme     - string
+
+for each hold:
+id - int
+x - int
+y - int
+rotation - int
+*/}
