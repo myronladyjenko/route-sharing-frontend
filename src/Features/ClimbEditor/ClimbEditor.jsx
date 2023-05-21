33e2ffd035e31b2c875abe22bceb5695693a7f4b
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-05-21 01:14:28 -0400

    created the climb editor feature

---
+++ src/Features/ClimbEditor/ClimbEditor.jsx
@@ -0,0 +1,50 @@
+import { createUseStyles } from "react-jss";
+
+export const ClimbEditor = () => {
+  const [holdType, setHoldType] = useState("none");
+  const [currentHolds, updateCurrentHolds] = useState([]); // [{id: 1, x: 20, y: 20, rotation: 90}
+  const [availableHolds, updateAvailableHolds] = useState([]); // [{id: 1, x: 20, y: 20, rotation: 90}
+
+  const updateHoldType = (newHoldType) => {
+    setHoldType(newHoldType);
+    updateAvailableHolds(
+      JSON.parse(sessionStorage.getItem("holds"))[newHoldType]
+    );
+  };
+
+  if (holdType === "none") {
+    console.log(JSON.parse(sessionStorage.getItem("holds")));
+    return (
+      <div>
+        <h1>Select a hold type to use</h1>
+        <div>
+          {Object.entries(JSON.parse(sessionStorage.getItem("holds"))).map(
+            ([holdType, arr], index) => {
+              return (
+                <button key={index} onClick={() => updateHoldType(holdType)}>
+                  {holdType}
+                </button>
+              );
+            }
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+
+  )
+};
+
+
+const useStyles = createUseStyles({
+    canvas: {
+        display: "flex",
+        width: "calc(700px + 10%)",
+        float: "right",
+        overflow: "hidden",
+        
+    }
+
+});
