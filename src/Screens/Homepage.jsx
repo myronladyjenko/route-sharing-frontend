e4686bd25ecfb35ea77a556ed0fc8df05252a051
TheAngryPopo <joelharder4@gmail.com>
2023-05-21 01:13:56 -0400

    selection list stuff

--- src/Screens/Homepage.jsx
+++ src/Screens/Homepage.jsx
@@ -1,11 +1,15 @@
 //this should be the main homepage?
 import "../shared-components/styles.css";
 import { createUseStyles } from "react-jss";
+
+import Lol from "../Components/HoldSelectionList";
+
 export function Home() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>CLIMBING FOR EVERYONE</h1>
       <button className="action-button">Browse Climbs</button>
+      <Lol placeHolder={"Select Something"}></Lol>
     </div>
   );
 }
